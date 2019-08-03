0a206384b27ad8da7f1b8bd652139d98362bb183
zoe-gonzales <zoegonzales03@gmail.com>
2019-08-03 13:08:47 -0600

    add schemas

---
+++ schemas.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS budget;
+CREATE DATABASE budget;
+USE budget;
+
+CREATE TABLE user_budgets (
+    budget_id INTEGER AUTO_INCREMENT NOT NULL,
+    budget_name VARCHAR(50) NOT NULL,
+    allowance DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY (budget_id)
+);
+
+CREATE TABLE transactions (
+    transaction_id INTEGER AUTO_INCREMENT NOT NULL,
+    transaction_desc VARCHAR(100) NOT NULL,
+    amount_spent DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY (transaction_id)
+);
