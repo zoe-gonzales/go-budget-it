a3cf2f99bb0ffe267cb96ed41abea1ba160ca153
zoe-gonzales <zoegonzales03@gmail.com>
2019-08-03 17:24:59 -0600

    initial queries

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 
@@ -18,14 +19,52 @@ func main() {
 	}
 	pass := os.Getenv("DB_PASS")
 	dsn := "root:" + pass + "@tcp(127.0.0.1:3306)/budget"
+
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		fmt.Println("Error in db registration:", err)
 		os.Exit(1)
 	}
+
 	err = db.Ping()
 	if err != nil {
 		fmt.Println("Error in db ping:", err)
 	}
-	fmt.Println("All is running as it should.....")
+
+	var q string
+
+	if os.Args[1] == "budget" {
+		q = "insert into user_budgets (budget_name, allowance) VALUES (?, ?)"
+	} else if os.Args[1] == "spent" {
+		q = "insert into transactions (transaction_desc, amount_spent) VALUES (?, ?)"
+	}
+
+	a, err := strconv.ParseFloat(os.Args[3], 64)
+	if err != nil {
+		fmt.Println("Error in string conversion to float:", err)
+	}
+	n := os.Args[2]
+
+	var (
+		id     int
+		name   string
+		amount float64
+	)
+
+	rows, err := db.Query(q, n, a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name, &amount)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name, amount)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
