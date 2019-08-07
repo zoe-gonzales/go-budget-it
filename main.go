546a7232058475a06e9f287b5fe9e929df39edea
zoe-gonzales <zoegonzales03@gmail.com>
2019-08-06 20:54:46 -0600

    update & delete actions

--- main.go
+++ main.go
@@ -44,6 +44,18 @@ func main() {
 		q = "select * from user_budgets"
 	} else if os.Args[1] == "get" && os.Args[2] == "transactions" {
 		q = "select * from transactions"
+	} else if os.Args[1] == "update" && os.Args[2] == "budget" {
+		q = "update user_budgets set allowance = ? where budget_id = ?"
+		m = "Budget successfully updated!"
+	} else if os.Args[1] == "update" && os.Args[2] == "transaction" {
+		q = "update transactions set amount_spent = ? where transaction_id = ?"
+		m = "Transaction successfully updated!"
+	} else if os.Args[1] == "delete" && os.Args[2] == "budget" {
+		q = "delete from user_budgets where budget_id = ?"
+		m = "Budget successfully deleted."
+	} else if os.Args[1] == "delete" && os.Args[2] == "transaction" {
+		q = "delete from transactions where transaction_id = ?"
+		m = "Transaction successfully deleted."
 	}
 
 	var (
@@ -52,7 +64,38 @@ func main() {
 		amount float64
 	)
 
-	if len(os.Args) == 4 {
+	// Update existing budget/transaction
+	if len(os.Args) == 5 {
+		a, err := strconv.ParseFloat(os.Args[3], 64)
+		if err != nil {
+			fmt.Println("Error in allowance conversion to float:", err)
+		}
+		id, err := strconv.ParseInt(os.Args[4], 10, 64)
+		if err != nil {
+			fmt.Println("Error in id conversion to integer:", err)
+		}
+
+		rows, err := db.Query(q, a, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			err := rows.Scan(&id, &amount)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println(id, amount)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(m)
+	}
+
+	// Insert a new budget/transaction
+	if len(os.Args) == 4 && os.Args[1] != "delete" {
 		a, err := strconv.ParseFloat(os.Args[3], 64)
 		if err != nil {
 			fmt.Println("Error in string conversion to float:", err)
@@ -75,7 +118,10 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Println(m)
-	} else if len(os.Args) == 3 {
+	}
+
+	// Select * from
+	if len(os.Args) == 3 {
 		rows, err := db.Query(q)
 		if err != nil {
 			log.Fatal(err)
@@ -93,5 +139,31 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+	}
+
+	// Delete a budget/transaction
+	if len(os.Args) == 4 && os.Args[1] == "delete" {
+		id, err := strconv.ParseFloat(os.Args[3], 64)
+		if err != nil {
+			fmt.Println("Error in allowance conversion to float:", err)
+		}
+
+		rows, err := db.Query(q, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			err := rows.Scan(&id, &amount)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println(id, amount)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(m)
 	}
 }
